ab8b2d2ea4c09c4c67ce7cdf5116506762e4e8ee
jokim3127 <josephkim3127@gmail.com>
2025-08-12 11:07:43 -0700

    Update Main.java

--- Initials/src/Main.java
+++ Initials/src/Main.java
@@ -11,13 +11,19 @@ public class Main implements Directions{
     // The line below creates a Robot that we will refer to as r.  
     // Find out what the numbers and direction do!
   
-		Robot f = new Robot(9,4,East,100);
-    Robot r = new Robot(9,4,East,100);
+		Robot f = new Robot(10,4,East,100);
+    Robot r = new Robot(10,4,East,100);
+    Robot g = new Robot(5,7,East,100);
+    Robot t = new Robot(6,7,East,100);
     // examples of commands you can invoke on a Robot
     f.move();
     f.turnLeft();
     f.turnLeft();
     f.turnLeft();
+    f.putBeeper();
+    f.move();
+    f.putBeeper();
+    f.move();
     f.putBeeper();
     f.move();
     f.putBeeper();
@@ -55,19 +61,69 @@ public class Main implements Directions{
     r.move();
     r.putBeeper();
     r.move();
-
     r.putBeeper();
     r.move();
-
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
     r.putBeeper();
     r.move();
     r.putBeeper();
     r.turnLeft();
     r.turnLeft();
 
+    g.move();
+    g.putBeeper();
+    g.move();
+    g.putBeeper();
+    g.turnLeft();
+    g.turnLeft();
+    g.turnLeft();
+    g.move();
+    g.putBeeper();
+    g.move();
+    g.putBeeper();
+    g.turnLeft();
+    g.move();
+    g.putBeeper();
+    g.move();
+    g.putBeeper();
+    g.turnLeft();
+    g.turnLeft();
+    g.turnLeft();
+    g.move();
+    g.putBeeper();
+    g.move();
+    g.putBeeper();
 
-
-
+    t.move();
+    t.putBeeper();
+    t.move();
+    t.putBeeper();
+    t.turnLeft();
+    t.move();
+    t.putBeeper();
+    t.move();
+    t.putBeeper();
+    t.turnLeft();
+    t.turnLeft();
+    t.turnLeft();
+    t.move();
+    t.putBeeper();
+    t.move();
+    t.putBeeper();
+    t.turnLeft();
+    t.move();
+    t.putBeeper();
+    t.move();
+    t.putBeeper();
 
 	f.putBeeper();
 
